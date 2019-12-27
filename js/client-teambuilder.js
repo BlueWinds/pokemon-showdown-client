361ce8b0653394ae069751852ba4d7b7bff6ac65
BlueWinds <admin@winds.blue>
2019-12-27 13:31:27 -0800

    Fix inconsistent smogon set import behavior between localdev and production

--- js/client-teambuilder.js
+++ js/client-teambuilder.js
@@ -1472,6 +1472,9 @@
 				this.importSetButtons();
 				return;
 			}
+			// We fetch this as 'text' and JSON.parse it ourserves in order to have consistent behavior
+			// between the localdev CORS helper and the real jQuery.get function, which would already parse
+			// this into an object based on the content-type header.
 			$.get('https://play.pokemonshowdown.com/data/sets/' + format + '.json', {}, function (data) {
 				try {
 					self.smogonSets[format] = JSON.parse(data);
@@ -1481,7 +1484,7 @@
 					self.smogonSets[format] = false;
 				}
 				self.importSetButtons();
-			});
+			}, 'text');
 		},
 		importSetButtons: function () {
 			var formatSets = this.smogonSets[this.curTeam.format];
